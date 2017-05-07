85d4741456fb91c33442d46789278412fae80807
Pranjal Sahu <pranjalsahu5@gmail.com>
2017-05-07 14:05:30 -0400

    garbage removal

--- clustering.py
+++ clustering.py
@@ -14,20 +14,16 @@ from matplotlib import pyplot as plt
 import os
 
 def extract_features():
-    textfeatures = [];
+    textfeatures = []
     names = [os.path.basename(x) for x in glob.glob('./dataset/feature/*')]
     files = glob.glob("./dataset/feature/*")
     nameCounter = 0
-    # iterate over the list getting each file 
     for fle in files:
-       # open the file and then call .read() to get the text
        with open(fle) as f:
           text = f.read()
           textfeatures.append(text)
-          
     allFeatures = []
-    len_hash   = {}
-    #len_hash['post'] = []
+    len_hash    = {}
     feature_type_array = []
     for feat in textfeatures:
         eachThreeSet = feat.split('\n')[0:3]
@@ -49,12 +45,14 @@ def extract_features():
     for key in all_keys:
         mean_hash[key] = sum(len_hash[key])/len(len_hash[key])
     all_features_filtered = []
+    all_features_filtered_type = []
     index = 0
     for feature in  allFeatures:
         if len(feature[2]) >= mean_hash[feature_type_array[index]]:
             all_features_filtered.append(feature)
+            all_features_filtered_type.append(mean_hash[feature_type_array[index]])
         index = index+1
-    return all_features_filtered
+    return [all_features_filtered, all_features_filtered_type, feature_type_array]
 
 
 def get_dtw_distance(flow1, flow2):
